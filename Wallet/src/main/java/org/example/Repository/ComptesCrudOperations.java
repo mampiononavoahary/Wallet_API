d86f45d3046472471a7b897e842d7c5ec876912f
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-04 21:34:04 +0300

    feat: find all comptes and transactions

--- Wallet/src/main/java/org/example/Repository/ComptesCrudOperations.java
+++ Wallet/src/main/java/org/example/Repository/ComptesCrudOperations.java
@@ -1,11 +1,40 @@
 package org.example.Repository;
 
+import org.example.Database.ConnectDatabase;
+import org.example.Model.Comptes;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComptesCrudOperations implements CrudOperation{
+    ConnectDatabase connectDatabase = new ConnectDatabase();
+    Connection connection = connectDatabase.CreateConnection();
+
+    public ComptesCrudOperations() throws SQLException, ClassNotFoundException {
+    }
+
     @Override
-    public List findAll() {
-        return null;
+    public List<Comptes> findAll() {
+        List<Comptes> array = new ArrayList<>();
+        String sql = "SELECT * FROM Comptes;";
+        try(PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                array.add(new Comptes(
+
+                ));
+            }
+            for (Comptes comptes : array){
+                System.out.println(comptes);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return array;
     }
 
     @Override
--- Wallet/src/main/java/org/example/Repository/TransactionsCrudOperations.java
+++ Wallet/src/main/java/org/example/Repository/TransactionsCrudOperations.java
@@ -1,11 +1,42 @@
 package org.example.Repository;
 
+import org.example.Database.ConnectDatabase;
+import org.example.Model.Comptes;
+import org.example.Model.Transactions;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionsCrudOperations implements CrudOperation{
+
+    ConnectDatabase connectDatabase = new ConnectDatabase();
+    Connection connection = connectDatabase.CreateConnection();
+
+    public TransactionsCrudOperations() throws SQLException, ClassNotFoundException {
+    }
+
     @Override
-    public List findAll() {
-        return null;
+    public List<Transactions> findAll() {
+        List<Transactions> array = new ArrayList<>();
+        String sql = "SELECT * FROM Comptes;";
+        try(PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                array.add(new Transactions(
+
+                ));
+            }
+            for (Transactions transactions : array){
+                System.out.println(transactions);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return array;
     }
 
     @Override
