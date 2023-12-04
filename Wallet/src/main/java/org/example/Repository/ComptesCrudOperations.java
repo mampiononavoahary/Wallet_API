f398d9a94daf8a51c7579e4afd7356c160eb1557
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-04 19:48:34 +0300

    feat:add ComptesCrudOperations

---
+++ Wallet/src/main/java/org/example/Repository/ComptesCrudOperations.java
@@ -0,0 +1,25 @@
+package org.example.Repository;
+
+import java.util.List;
+
+public class ComptesCrudOperations implements CrudOperation{
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public List saveAll(List toSave) {
+        return null;
+    }
+
+    @Override
+    public Object save(Object toSave) {
+        return null;
+    }
+
+    @Override
+    public Object delete(Object toDelete) {
+        return null;
+    }
+}
--- Wallet/src/main/java/org/example/Repository/ComptesRepository.java
+++
@@ -1,2 +0,0 @@
-package org.example.Repository;public class ComptesRepository {
-}
