fe9944f756b90c8b7daffdd428fd3b6e8760b13a
Voahary Mampionona <hei.zoarisoa@gmail.com>
2023-12-05 00:10:26 +0300

    add transactions

--- Wallet/src/main/java/org/example/Repository/TransactionsCrudOperations.java
+++ Wallet/src/main/java/org/example/Repository/TransactionsCrudOperations.java
@@ -43,17 +43,79 @@ public class TransactionsCrudOperations implements CrudOperation<Transactions>{
     }
 
     @Override
-    public Transactions save(Transactions toSave) {
+    public List<Transactions> saveAll(List<Transactions> toSave) {
+        String sql = "INSERT INTO Transactions(id_transaction, id_compte, montant, type_transaction, date_transaction) VALUES (?, ?, ?, ?, ?, ?);";
+        List<Transactions> array = new ArrayList<>();
 
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            for (Transactions transaction : toSave) {
+                statement.setInt(1, transaction.getId_transaction());
+                statement.setInt(2, transaction.getId_compte());
+                statement.setFloat(3, transaction.getMontant());
+                statement.setString(4, transaction.getType_transaction());
+                statement.setTimestamp(5, transaction.getDate_transaction());
+
+                int rows = statement.executeUpdate();
+                if (rows > 0) {
+                    System.out.println("Transaction inserted");
+                    array.add(transaction);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return array;
     }
 
+
     @Override
-    public Transactions delete(Transactions toDelete) {
+    public Transactions save(Transactions toSave) {
+        String sql = "INSERT INTO Transaction(id_transaction, id_compte, montant, type_transaction, date_transaction, description) VALUES (?, ?, ?, ?, ?, ?);";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, toSave.getId_transaction());
+            statement.setInt(2, toSave.getId_compte());
+            statement.setFloat(3, toSave.getMontant());
+            statement.setString(4, toSave.getType_transaction());
+            statement.setTimestamp(5, toSave.getDate_transaction());
+
+            int rows = statement.executeUpdate();
+            if (rows > 0) {
+                System.out.println("Transaction insérée avec succès");
+
+                // Récupérer les clés générées automatiquement (si applicable)
+                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        toSave.setId_transaction(generatedKeys.getInt(1));
+                    }
+                }
+                return toSave;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 
+
     @Override
-    public List saveAll(List toSave) {
+    public Transactions delete(Transactions toDelete) {
+        String sql = "DELETE FROM Transactions WHERE id_transaction = ?;";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, toDelete.getId_transaction());
+            int rows = statement.executeUpdate();
+
+            if (rows > 0) {
+                System.out.println("Transaction supprimée avec succès");
+                return toDelete;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 
---
+++ Wallet/src/main/java/org/example/Tests/ComptesTest.java
@@ -0,0 +1,2 @@
+package org.example.Tests;public class ComptesTest {
+}
